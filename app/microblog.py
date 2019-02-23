114ff187e283faebf829d0c08c08c3a666a04a1b
kanyu <kanyuds@gmail.com>
2019-02-23 19:26:19 +0700

    Reset Project
    Reset Project

--- app/microblog.py
+++
@@ -1,9 +0,0 @@
-from app import appp, db
-from app.models import User, Post
-
-@appp.shell_context_processor
-def make_shell_context():
-    return {'db': db,
-            'User': User,
-            'Post': Post
-            }
--- app/temp.py
+++ app/temp.py
@@ -1,49 +1,10 @@
-from flask import render_template, flash, redirect, url_for, request
-from flask_login import login_user, logout_user, current_user, login_required
-from werkzeug.urls import url_parse
-from app import appp, db
-from app.forms import LoginForm
-from app.models import User
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///app.db' or (
+            'sqlite:///' + os.path.join(basedir, 'app.db'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-@appp.route('/')
-@appp.route('/index')
-@login_required
-def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
-
-
-@appp.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
-
-
-@appp.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
