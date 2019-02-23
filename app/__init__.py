9fd3057b3cd1fd72ed6d0c5e68d368dde63c6835
kanyu <kanyuds@gmail.com>
2019-02-23 19:56:24 +0700

    Database model
    Database model

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 myApp = Flask(__name__)
 myApp.config.from_object(Config)
+db = SQLAlchemy(myApp)
+migrate = Migrate(myApp, db)
 
 from app import routes
--- app/microblog.py
+++ app/microblog.py
@@ -1,1 +1,7 @@
-from app import myApp
+from app import myApp, db
+from app.models import User, Post
+
+
+@myApp.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
