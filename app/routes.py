3f809c4ff61b0fb1d9686ad76d78cee523b434b3
kanyu <kanyuds@gmail.com>
2019-02-23 19:37:06 +0700

    Create Template
    Create Template

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import myApp
 
 
 @myApp.route('/')
 @myApp.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Kan'}
+    posts = [
+        {
+            'author': {'username': 'Thor'},
+            'body': 'Beautiful Thailand!'
+        },
+        {
+            'author': {'username': 'Nano'},
+            'body': 'The Alita movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
