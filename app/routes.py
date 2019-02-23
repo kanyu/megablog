ac706d56b2813f3d53dd4e781cd13cacf15d3c33
kanyu <kanyuds@gmail.com>
2019-02-23 21:22:34 +0700

    add register function
    add register function

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,8 @@ from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from app.models import User
-from app.forms import LoginForm
-from app import myApp
+from app.forms import LoginForm, RegistrationForm
+from app import myApp, db
 
 print("==================BLOG START=====================")
 import config
@@ -18,7 +18,6 @@ print("=================================================")
 @myApp.route('/index')
 @login_required  # this decorator to protect page which need login
 def index():
-    user = {'username': 'Kan'}
     posts = [
         {
             'author': {'username': 'Thor'},
@@ -29,7 +28,7 @@ def index():
             'body': 'The Alita movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', posts=posts)
 
 
 @myApp.route('/login', methods=['GET', 'POST'])
@@ -55,7 +54,23 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @myApp.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@myApp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
